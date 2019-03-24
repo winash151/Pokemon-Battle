4bae4be96ea21f34c63151178b0d50c2a9d5a4f6
winash151 <ashksuresh@umass.edu>
2019-03-24 13:10:40 -0400

    more changes

--- src/RunThisOne.java
+++ src/RunThisOne.java
@@ -4,7 +4,7 @@ import networking.PokemonServer;
 
 public class RunThisOne{
 
-	private static final long serialVersionUID = 8831070381585038171L;
+//	private static final long serialVersionUID = 8831070381585038171L;
 	
 	public static void main(String [] args) {
 		PokemonServer.main(args);
